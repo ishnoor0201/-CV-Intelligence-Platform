a4c4af2c4a16a10f1039b1b43cec9d7edda2037d
ishnoor0201 <76932129+ishnoor0201@users.noreply.github.com>
2025-09-26 11:11:13 +0530

    Add files via upload

---
+++ AI-Resume-Analyzer-main/App/tempCodeRunnerFile.py
@@ -0,0 +1,6 @@
+resume_videos = [ 'https://youtu.be/Tt08KmFfIYQ','https://youtu.be/y8YH0Qbu5h4',
+                  'https://youtu.be/u75hUSShvnc','https://youtu.be/BYUy1yvjHxE',
+                  'https://youtu.be/KFaugkGVeNQ','https://youtu.be/3agP4x8LYFM',
+                  'https://youtu.be/GyjzOKdaioU','https://youtu.be/17YZBH_qtmg',
+                  'https://youtu.be/C7pNLIq3kOI','https://youtu.be/xpaz7nrNmXA',
+                  'https://youtu.be/aKjsy-b00QM','https://youtu.be/ciIkiWwZnlc']
